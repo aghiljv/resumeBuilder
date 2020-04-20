2d2729b8406d405c632ded997cb511b322f881c3
aghiljv <aghiljv@gmail.com>
2020-04-20 19:29:35 +0530

    updates

--- src/components/Section.js
+++ src/components/Section.js
@@ -20,18 +20,18 @@ function Section() {
 		<div>
 			<div className="row">
 				<div className="col-sm-5 indSection" onClick={() => setActiveSection('basic')}>
-					<img src={require('../images/basic.png')} className="iconStyle" alt="basic" />
+					<img src={require('../images/basic.png')} className=" img-fluid" alt="basic" />
 				</div>
 				<div className="col-sm-5 indSection" onClick={() => setActiveSection('education')}>
-					<img src={require('../images/education.png')} className="iconStyle" alt="education" />
+					<img src={require('../images/education.png')} className=" img-fluid" alt="education" />
 				</div>
 			</div>
 			<div className="row">
 				<div className="col-sm-5 indSection" onClick={() => setActiveSection('experience')}>
-					<img src={require('../images/experience.png')} className="iconStyle" alt="experience" />
+					<img src={require('../images/experience.png')} className=" img-fluid" alt="experience" />
 				</div>
 				<div className="col-sm-5 indSection" onClick={() => setActiveSection('skills')}>
-					<img src={require('../images/skill.png')} className="iconStyle" alt="skills" />
+					<img src={require('../images/skill.png')} className=" img-fluid" alt="skills" />
 				</div>
 			</div>
 		</div>
